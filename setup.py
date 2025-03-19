81a2f1361f1afcd9dc051d3b6d778e3e40fd82ff
Satyam Palkar <73163418+SatyamPalkar@users.noreply.github.com>
2025-03-19 23:58:16 +1100

    New updated

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import setuptools
 
-with open("READ.md", "r", encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
     
-
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer"
